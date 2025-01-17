d4afc6b5b4a79eb05002f181dc52053abf7a35b8
SchCyril <schneidenbach.cyril@gmail.com>
2025-01-17 15:23:17 +0100

    new version

--- customer-service/src/main/java/fr/cyril/config/ConfigTestController.java
+++ customer-service/src/main/java/fr/cyril/config/ConfigTestController.java
@@ -3,12 +3,14 @@ package fr.cyril.config;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 // Second Way
 @RestController
+@RefreshScope
 public class ConfigTestController {
 
 	@Value("${global.params.p1}")
